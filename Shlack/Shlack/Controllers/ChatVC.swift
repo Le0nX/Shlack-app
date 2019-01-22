e8bca8ee42d6d0092d23a6c1cce6f9947a74907e
Le0nX <cloudmindone@gmail.com>
2019-01-22 23:42:00 +0300

    fetaure: added button action

--- Shlack/Shlack/Controllers/ChatVC.swift
+++ Shlack/Shlack/Controllers/ChatVC.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class ChatVC: UIViewController {
 
+    @IBOutlet weak var menuBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // button settings
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
 
-        // Do any additional setup after loading the view.
+     
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
